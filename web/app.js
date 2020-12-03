3316f1b9847b8e2d5f33901f657b28ee4a3a71cb
almudena casajus izquierdo <168036@iessierradeguara.com>
2020-12-03 16:39:38 +0100

    clase

---
+++ web/app.js
@@ -0,0 +1,23 @@
+console.log("hola mundo")
+
+let texto = "Esto es un texto";
+let numero = 25 ;
+let decimal = 5.4;
+let booleano = true ;
+
+let array = [ "Jose", 58, "Moreno"];
+
+let persona ={ 
+    "nombres": "Manuel",
+    "edad": 23,
+    "cabello": "Rubio"
+}
+
+console.log (texto);
+console.log(numero);
+console.log(array)
+console.log (decimal);
+console.log(booleano);
+console.log(persona.nombre);
+console.log(persona["nombre"])
+console.log(personaArray[1])
